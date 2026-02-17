7d733674652cb8dc7f789ad517f5179a0aafca9d
Matt Bussing <mbussing44@gmail.com>
2026-02-16 21:16:13 -0600

    main - create a dependent project

---
+++ 2026-02-09-Nerdbank.GitVersioning/project2/SampleClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Project2
+{
+    public class SampleClass
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Constructor
+        public SampleClass(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Method
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}");
+        }
+    }
+}
